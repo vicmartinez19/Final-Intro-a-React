0530cd7f010042280ea87bfe9981d9730be98183
vicmartinez <vmmartinez.infomec19@gmail.com>
2026-09-13 23:19:18 -0600

    feat: inicializar proyecto Twitter Clon con Vite y React Router

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
+
+/**
+ * HashRouter para soporte universal en GitHub Pages sin errores 404 al recargar.
+ */
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
+);
